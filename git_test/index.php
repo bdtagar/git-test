1527a329f816b8c69fb07904854549733a049da1
Badar <Badar@moon>
2016-02-18 20:11:53 +0500

    Adding files to repo by commiting from Netbeans.

---
+++ git_test/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Date</title>
+    </head>
+    <body>
+        <?php
+        echo date('j F Y');
+        ?>
+        <br />
+        <?php
+        $people = array('Badar', 'Ali', 'Ahmed', 'Tagar');
+        foreach ($people as $value) {
+            echo "Person: " . $value . "<br />";
+        }
+        ?>
+        <br />
+        <a href="page.php">open page</a>     
+    </body>
+</html> 
